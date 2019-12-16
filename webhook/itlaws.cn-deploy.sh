f000ccb1ef71f7bd561bc2cc1f4f2bb151ea608f
Choicky ZHOU <zhoucaiqi@gmail.com>
2019-12-16 22:19:43 +0800

    update deploy.sh

--- webhook/itlaws.cn-deploy.sh
+++ webhook/itlaws.cn-deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-cd /var/www/itlaws.cn && git pull --recurse-submodules && rm -rf public && hugo
+# 网站的代码仓库目录
+input="/var/www/itlaws.cn"
+
+# Nginx 中配置的网站的 HTML 根目录
+output="/var/www/itlaws.cn/public"
+
+cd $input && git pull --recurse-submodules && hugo --cleanDestinationDir --destination=/var/www/itlaws.cn/public
+
