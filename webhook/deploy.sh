ece801fe4ab86c12d9a15508ac52c4897780f4b3
Choicky ZHOU <zhoucaiqi@gmail.com>
2019-12-17 23:21:07 +0800

    update webhook

--- webhook/deploy.sh
+++ webhook/deploy.sh
@@ -3,10 +3,20 @@
 # 配合 npm i -S github-webhook-handler 使用
 
 # 网站的代码仓库目录
-input="/var/www/itlaws.cn"
+repo_path="/var/www/itlaws.cn"
+USER='www-data'
+USERGROUP='www-data'
 
 # Nginx 中配置的网站的 HTML 根目录
-output="/var/www/itlaws.cn/public"
+web_root="/var/www/itlaws.cn/public"
 
-cd $input && git pull --recurse-submodules && hugo --cleanDestinationDir --destination=/var/www/itlaws.cn/public
+# 更新 repo 并生成新的静态化文件
+cd $repo_path
+git reset --hard origin/master
+git clean -f
+git pull --recurse-submodules
+git checkout master
+chown -R $USER:$USERGROUP $repo_path
+hugo -d $web_root
+
 
