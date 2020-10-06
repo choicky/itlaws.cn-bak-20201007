f0300367be6ad5fd7afe4795802f74f79e154d06
Choicky ZHOU <zhoucaiqi@gmail.com>
2020-10-06 10:08:00 +0800

    Update deploy.sh

--- webhook/deploy.sh
+++ webhook/deploy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
+# work with webhook (https://github.com/adnanh/webhook)
 
-# 配合 npm i -S github-webhook-handler 使用
-
-# 网站的代码仓库目录
-repo_path="/var/www/itlaws.cn"
+# user config
 USER='www-data'
 USERGROUP='www-data'
 
-# Nginx 中配置的网站的 HTML 根目录
+# path config
+repo_path="/var/www/itlaws.cn"
 web_root="/var/www/itlaws.cn/public"
 
-# 更新 repo 并生成新的静态化文件
+# git pull and update submodules
 cd $repo_path
-# git reset --hard origin/master
 git clean -f
-git pull --recurse-submodules
-# git checkout master
+git pull
+git submodule update --remote
+
+# regenerate html with hugo
 chown -R $USER:$USERGROUP $repo_path
 hugo -s $repo_path -d $web_root
